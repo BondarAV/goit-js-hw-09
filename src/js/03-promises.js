e092da84cb407efdb1b48be355c457fa93721177
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-29 13:06:02 +0200

    Completes task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let currentDelay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+
+  for (let i = 0; i < Number(amountInput.value); i++) {
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
+
+    currentDelay += step;
+  }
+});
